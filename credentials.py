fda1f4b2a0676c389351d9b2128f172414047645
Nandan Varma <137528244+NandanUnv@users.noreply.github.com>
2023-10-02 22:04:14 +0530

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,12 @@
+# person 1
+user_name = Abhi
+password = 8073
+# person 2
+user_name = Balu
+password = 35535
+# person 3
+user_name = Chandini
+password = 2727
+# person 4
+user_name = Danush
+password = 9066
